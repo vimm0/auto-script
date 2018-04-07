3d16504b08c5e8366b83f7d0660cd2ffe3d98701
Sandesh Rana <vimmrana0@gmail.com>
2018-04-07 20:07:29 +0545

    Update upload_file_aws.py

--- upload_file_aws.py
+++ upload_file_aws.py
@@ -2,12 +2,12 @@ import boto3
 from botocore.client import Config
 s3 = boto3.resource(
     's3',
-    aws_access_key_id='AKIAIGBYCQLUT7CCVVDA',
-    aws_secret_access_key='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',
+    aws_access_key_id='XXX',
+    aws_secret_access_key='XXX',
     config=Config(signature_version='s3v4')
 )
 
 # SEND_MEDIA_ROOT = '/home/vimm0/office/media/video/cut_71XwINQ.mp4'
 # data = open(SEND_MEDIA_ROOT, 'rb')
 
-# s3.Bucket('jus-backend-static-dev').put_object(Key='/media/video/cut_71XwINQ.mp4', Body=data, ACL='public-read')
+# s3.Bucket('XXX-backend-static-dev').put_object(Key='/media/video/cut_71XwINQ.mp4', Body=data, ACL='public-read')
